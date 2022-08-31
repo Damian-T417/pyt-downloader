2352d9cc3c6b2cea94acc423281c1dec30319942
Damian-T417 <demitru471@gmail.com>
2022-08-31 15:38:48 -0500

    PytDownloader v0.1

--- PytDownloader.py
+++ PytDownloader.py
@@ -1,26 +1,39 @@
+'''PytDownloader v0.1 using pytube library and tkinter, Made By Damian-T417'''
+
 # Importing packages
 import os
 
 from tkinter import *
-from tkinter.simpledialog import askstring
 from tkinter import messagebox
 from pytube import YouTube
 
 
-class YoutubeDownloader(Frame):
+class YoutubeDownloader(Tk):
 
-    # Constructor
-    def __init__(self, master, *args, **kwargs):
-        Frame.__init__(self, master, *args, **kwargs)
-        self.parent = master
-        self.grid()
+    # Window Constructor
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+
+        # Configuring the window and grid (final desing)
+        # self.geometry("651x424")
+
+        self.geometry("300x150")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.title("Youtube Downloader")
+        self.resizable(False, False)
         self.createWidgets()
 
     # Functionality of the program
     def downloadLink(self, option):
-        print(self.link.get(), option)
-        yt = YouTube(self.link.get())
-        
+        # Debuging
+        # print(self.link.get(), option)
+        try:
+            yt = YouTube(self.link.get())
+        except(Exception):
+            return messagebox.showerror("Syntax error", "The link is not supported or is misspelled")
+
         # audio option validation
         if (option == 0):
             video = yt.streams.filter(only_audio=True).first()
@@ -28,8 +41,11 @@ class YoutubeDownloader(Frame):
         if (option == 1):
             video = yt.streams.filter(res="720p").first()
 
-        destination = askstring('Save file', 'Enter the destination for save (leave blank for current directory)')
+        # Error 'Label' object is not callable (why?)
+        # destination = askstring('Save file', 'Enter the destination for save (leave blank for current directory)')
 
+        # For the moment the destination will save in the current directory until the v0.2
+        destination = ""
         if (destination == ""):
             destination = '.'
 
@@ -43,33 +59,28 @@ class YoutubeDownloader(Frame):
             new_file = base + '.mp4'
 
         os.rename(out_file, new_file)
-        self.link.delete(0, END)
+        self.link.delete(0, 'end')
 
         messagebox.showinfo("Complete download", yt.title + " has been successfully downloaded")
-
-
 
     # Creation the graphic interface
     def createWidgets(self):
         option_download = IntVar()
 
-        self.title = Label(self, text="Youtube Video downloader", justify=CENTER)
-        self.title.grid(row=0, column=0, columnspan=2, sticky="nsew")
+        self.title = Label(self, font=12, text="Youtube Video downloader")
+        self.title.grid(row=0, column=0, columnspan=2, padx=1, ipady=6)
 
-        self.link = Entry(self, justify=LEFT)
-        self.link.grid(row=1, column=0, sticky="nsew", columnspan=2)
+        self.link = Entry(self)
+        self.link.grid(row=1, column=0, columnspan=2, rowspan=2, padx=10, pady=5, sticky=W+E)
 
-        self.audio = Radiobutton(self, text="Audio", justify=CENTER, variable=option_download, value=0)
-        self.audio.grid(row=2, column=0, sticky="nsew")
+        self.audio = Radiobutton(self, text="Audio", variable=option_download, value=0)
+        self.audio.grid(row=3, column=0)
 
-        self.video = Radiobutton(self, text="Video", justify=CENTER, variable=option_download, value=1)
-        self.video.grid(row=2, column=1, sticky="nsew")
+        self.video = Radiobutton(self, text="Video", variable=option_download, value=1)
+        self.video.grid(row=3, column=1)
 
-        self.download = Button(self, text="Download", justify=CENTER, command=lambda: self.downloadLink(option_download.get()))
-        self.download.grid(row=3, column=0, sticky="nsew", columnspan=2)
+        self.download = Button(self, height=2, text="Download", command=lambda: self.downloadLink(option_download.get()))
+        self.download.grid(row=4, column=0, columnspan=2, rowspan=6, padx=10, pady=5, sticky=S+N+E+W)
 
-downloader = Tk()
-downloader.title("Youtube Downloader")
-downloader.resizable(False, False)
-root = YoutubeDownloader(downloader)
-downloader.mainloop()
+root = YoutubeDownloader()
+root.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
+# PytDownloader v0.1 functions summary
+PytDownloader is a GUI for downloading videos of Youtube, using Pytube library as functionality and
+tkinter for UI.
+<br>
+
+## How to use
+.- Put the link of your video or audio in the text field.
+.- Select the format that download (if press video button, they will download in 720p quality if exist).
+.- Finally press download button and the program start to download the video.
+
+## In this version
+.- PytDownloader hasn't executable, only can run with the command "python" and the file PytDownloader.py ("python pytDownloader.py").
+.- Only download your video or audio in the current directory because a bug that will solve in the next version.
+.- PytDownloader can download videos in 720p and if you wish only audio.
+
+That all for this version, see you in the next **version** :)
+
+*Notes*
 This repository is for practice graphic interfaces in python.
-Nothing important yet, probably in the future
+Nothing important yet, probably more in the future
