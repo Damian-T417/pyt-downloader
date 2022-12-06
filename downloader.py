8c60ec40723d2b8b03ddccf8de2750112aab675c
Damián Trujillo <demitru471@gmail.com>
2022-12-06 00:15:28 -0600

    Add table and search functionality

--- downloader.py
+++ downloader.py
@@ -6,6 +6,8 @@ import moviepy.editor as mp
 
 from tkinter import ttk
 from tkinter import S, N, E, W
+from tkinter import CENTER
+from tkinter import END
 from tkinter import messagebox
 from pytube import YouTube
 from save import Savefile
@@ -18,63 +20,44 @@ class Pytdownloader(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
 
         # Configuring the window and grid
-        self.geometry("500x425")
+        self.title("Youtube Downloader")
+        self.geometry("500x415")
+        self.resizable(False, False)
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
 
-        self.title("Youtube Downloader")
-        self.resizable(False, False)
+        self.file_title = tk.StringVar()
+        self.file_title.set("File name")
+
         self.create_widgets()
+        self.create_table()
 
-    def download_file(self, itag):
-        print("Yes, it's that a function? ", itag)
-        Savefile()
-
-    # Functionality of the program
-    def print_list(self):
+    def get_files(self, link, option):
 
         try:
-            yt = YouTube(self.link.get())
+            yt = YouTube(link)
         except Exception:
             return messagebox.showerror(
                 "Syntax error",
                 "The link is not supported or misspelled"
             )
 
-        files = yt.streams
-        # Title
-        self.title = tk.Label(self, font=12, pady=10, text=yt.title)
-        self.title.grid(row=2, column=0, columnspan=5)
-        i = 0
+        if option == 0:
+            streams = yt.streams.filter(only_audio=True)
+        elif option == 1:
+            streams = yt.streams.filter(only_video=True)
 
-        # Table
-        for file in files:
+        self.file_title.set(yt.title)
+        self.table.delete(*self.table.get_children())
+        self.link.delete(0, 'end')
 
-            itagVar = tk.IntVar()
-            # Debugging
-            print(file)
-            #itagVar.set(i.itag)
+        for file in streams:
+            self.table.insert("",END,text=file.itag, values=(file.mime_type, file.resolution, file.codecs))
 
-            # itag
-            # self.itag = tk.Entry(self, font=5, state="disabled", textvariable=itagVar)
-            self.itag = tk.Label(self, font=5, text=file.itag)
-            self.itag.grid(row=9 + i, column=0, sticky=E+W)
-
-            # mime_type
-            self.mime = tk.Label(self, font=1, anchor=W, padx=10, text="audio/webm")
-            self.mime.grid(row=9 + i, column=1, sticky=E+W)
-
-            # res (resolution)
-            # If this is audio, don't show
-            self.res = tk.Label(self, font=5, text="720p")
-            self.res.grid(row=9 + i, column=2, sticky=E+W)
-
-            # download
-            self.download = tk.Button(self, width=5, text="Download", command=lambda: self.download_file(self.itag.cget("text")))
-            self.download.grid(row=9 + i, column=3, padx=10, sticky=E+W)
-
-            i=+1
-
+    def download_file(self, selected):
+        itag = self.table.item(selected, 'text')
+        print("Downloading: ", itag)
 
     # Creation the graphic interface
     def create_widgets(self):
@@ -85,8 +68,8 @@ class Pytdownloader(tk.Tk):
         self.link = tk.Entry(self)
         self.link.grid(row=0, column=0, pady=10, padx=10, columnspan=3, sticky=W+E)
 
-        self.search = tk.Button(self, width=10, text="Search", command=lambda: self.print_list())
-        self.search.grid(row=0, column=3, pady=10, padx=10, sticky=W+E)
+        self.search = tk.Button(self, width=10, text="Search", command=lambda: self.get_files(self.link.get(), option_download.get()))
+        self.search.grid(row=0, column=3, pady=10, padx=10, sticky=W)
 
         # Options
         self.audio = tk.Radiobutton(self, text="Audio", variable=option_download, value=0)
@@ -94,3 +77,34 @@ class Pytdownloader(tk.Tk):
 
         self.video = tk.Radiobutton(self, text="Video", variable=option_download, value=1)
         self.video.grid(row=1, column=1)
+
+        # Title
+        self.title = tk.Label(self, font=8, pady=10, textvariable=self.file_title)
+        self.title.grid(row=2, column=0, columnspan=3)
+
+        #Download button
+        self.download = tk.Button(self, text="Download", command=lambda:self.download_file(self.table.focus()))
+        self.download.grid(row=4, column=0, columnspan=3, padx=10, pady=10, sticky=W+E)
+
+    def create_table(self):
+
+        table_scroll = ttk.Scrollbar(self)
+        table_scroll.grid(row=3, column=3, sticky=N+S+W)
+
+        self.table = ttk.Treeview(self, yscrollcommand=table_scroll.set)
+
+        self.table['columns'] = ("Type", "Resolution", "Codec")
+
+        table_scroll.config(command=self.table.yview)
+
+        self.table.column("#0",width=80, anchor=CENTER, stretch=False)
+        self.table.column("Type",width=80, anchor=CENTER)
+        self.table.column("Resolution",width=80, anchor=CENTER)
+        self.table.column("Codec", width=80, anchor=CENTER)
+
+        self.table.heading("#0", text="ID", anchor=CENTER)
+        self.table.heading("Type", text="Type", anchor=CENTER)
+        self.table.heading("Resolution", text="Resolution", anchor=CENTER)
+        self.table.heading("Codec", text="Codec", anchor=CENTER)
+
+        self.table.grid(row=3, column=0, columnspan=3, padx=10, pady=10, sticky=E+W+N+S)
